f8687bfbf7d976bc76e0d59a29f267e5902fc82a
kmvishnu <kmvishnu1996@gmail.com>
2024-10-30 16:17:47 +0530

    updated styles

--- src/Components/SidebarComponent/SidebarComponent.js
+++ src/Components/SidebarComponent/SidebarComponent.js
@@ -1,18 +1,35 @@
-import React from 'react'
-import './SidebarComponent.css'
-import { useNavigate } from 'react-router-dom'
-
+import React, { useState } from 'react';
+import './SidebarComponent.css';
+import { useNavigate } from 'react-router-dom';
 
 const SidebarComponent = () => {
-
   const navigate = useNavigate();
+  const [activeButton, setActiveButton] = useState('/');
+
+  const handleButtonClick = (path) => {
+    setActiveButton(path);
+    navigate(path);
+  };
+
   return (
     <div className='sidebar'>
-        <button className='sidebarButton' onClick={()=>navigate('/')}>Home</button>
-        <button className='sidebarButton' onClick={()=>navigate('/todo')}>TodoApp</button>
-        <button className='sidebarButton' onClick={()=>navigate('/about')}>About</button>
+      <button 
+        className={`sidebarButton ${activeButton === '/' ? 'active' : ''}`} 
+        onClick={() => handleButtonClick('/')}>
+        Home
+      </button>
+      <button 
+        className={`sidebarButton ${activeButton === '/todo' ? 'active' : ''}`} 
+        onClick={() => handleButtonClick('/todo')}>
+        TodoApp
+      </button>
+      <button 
+        className={`sidebarButton ${activeButton === '/about' ? 'active' : ''}`} 
+        onClick={() => handleButtonClick('/about')}>
+        About
+      </button>
     </div>
-  )
+  );
 }
 
-export default SidebarComponent
+export default SidebarComponent;
